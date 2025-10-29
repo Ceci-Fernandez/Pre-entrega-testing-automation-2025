19eceedf5aad81b28833cb5683372dbae681cc7c
Cecilia <cecilia92fernandez@gmail.com>
2025-10-29 17:10:24 -0300

    Se creo helpers y metodo para esperar la url

---
+++ utils/helpers.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def esperar_url_contiene(driver, texto_url, timeout=10):
+    
+    try:
+        wait = WebDriverWait(driver, timeout)
+        wait.until(EC.url_contains(texto_url))
+        return True
+    except:
+        return False
+
